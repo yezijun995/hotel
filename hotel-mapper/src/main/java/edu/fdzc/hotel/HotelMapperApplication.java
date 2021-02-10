543eecb39ffdabd4dbb4f9b6125f87b67a9fd696
yezj <1016445037@qq.com>
2021-02-10 15:14:54 +0800

    init
    (cherry picked from commit a441b2a9869303db6fa4cf67ccf91f246ab13c1a)

---
+++ hotel-mapper/src/main/java/edu/fdzc/hotel/HotelMapperApplication.java
@@ -0,0 +1,13 @@
+package edu.fdzc.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelMapperApplication.class, args);
+    }
+
+}
---
+++ hotel-web/src/main/java/edu/fdzc/hotel/HotelWebApplication.java
@@ -0,0 +1,13 @@
+package edu.fdzc.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelWebApplication.class, args);
+    }
+
+}
